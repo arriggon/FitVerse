727075a35ec07e2caba612f55a2100b6a4dd266b
Moritz Brandstätter <arriggon@gmail.com>
2023-12-29 14:48:59 +0100

    add tailwindcss

---
+++ fitverse-frontend/src/components/WorkoutPane/WorkoutPane.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="space-y-5">
+    <h1 class="text-3xl lg:text-2xl text-white">Workout</h1>
+    <form>
+      <input type="email" class="form-input" placeholder="Name">
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- fitverse-frontend/src/views/HomeView.vue
+++ fitverse-frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import WorkoutPane from '../components/WorkoutPane/WorkoutPane.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <WorkoutPane />
   </main>
 </template>
